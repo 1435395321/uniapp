980736b84c641cb9f387babd5135b2109d83a358
wangmeng <1435395321@qq.com>
2020-11-11 18:18:09 +0800

    aa

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ import App from './App'
 // 引入uView
 import uView from "uview-ui";
 Vue.use(uView);
+
 //引入vuex
 import store from './store'
-
 //把vuex定义成全局组件
 Vue.prototype.$store = store
+
+// 引入echart
+import echarts from 'echarts'
+//把vuex定义成全局组件
+Vue.prototype.$echarts = echarts
+Vue.use(echarts);
 
 Vue.config.productionTip = false
 
--- pages/tabBar/home/home.vue
+++ pages/tabBar/home/home.vue
@@ -1,11 +1,36 @@
 <template>
-	<view class="view">
-		view
+	<view class="wrap">
+		<u-swiper :effect3d="true" :list="list"></u-swiper>
 	</view>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				list: [{
+						image: '/static/image/banner/lol3.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '/static/image/banner/lol1.jpg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					},
+					{
+						image: '/static/image/banner/lol.jpg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}
+				],
+			}
+		},
+		methods: {
+
+		}
+	}
 </script>
 
-<style>
+<style scoped lang="scss">
+	.wrap {
+		padding: 40rpx;
+	}
 </style>
